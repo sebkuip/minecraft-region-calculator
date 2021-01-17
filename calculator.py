63a6d5d62c20d1348073054a4f4f8091c7858184
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2021-01-17 19:10:04 +0100

    yes

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 # A simple calculator to see the region/chunks/blocks depending on coordinates
 # You just enter your data in the format `T.X.Z`, with T being Type (B/C/R for Block, Chunk or Region respectively) and X and Z being the coordinates of the thing you want to calculate
 # When given a block type, the format also allows for entering a Y coordinate following the format `B.X.Y.Z`
+# The . may also be replaced by a , or a space.
 # When given a block it will give you the chunk and region coordinates this block is in
 # When given a chunk it shows what blocks it contains, and what region it is in
 # When given a region it shows the coordinates of the blocks it contains and the coordinates of the chunks it contains
